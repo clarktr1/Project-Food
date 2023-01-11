6266cf2db46cac05e35d05b616088f87dc9a05b3
Kiefer King <kiefydking@yahoo.com>
2023-01-11 17:19:00 -0600

    Added a function to save ingredients to the list

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,6 @@
-
+function addIngredient(event) {
+    event.preventDefault();
+    var ingredient = document.getElementById("ingredient-input").value;
+    var list = document.getElementById("list-of-ingrdients");
+    list.innerHTML += ingredient + "<br>";
+  }
