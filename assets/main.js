9dbe1490899e5994e5a379260aa76d71981ae7f8
Kiefer King <kiefydking@yahoo.com>
2023-01-11 17:27:34 -0600

    added a line to clear the input and a comment

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,8 @@
+//  Adds the ingredient into the list as well as clears the bar for the next input
 function addIngredient(event) {
     event.preventDefault();
     var ingredient = document.getElementById("ingredient-input").value;
     var list = document.getElementById("list-of-ingrdients");
     list.innerHTML += ingredient + "<br>";
+    document.getElementById("ingredient-input").value = "";
   }
