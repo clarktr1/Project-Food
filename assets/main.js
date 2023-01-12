25c7a3a7f8cc1bdefa9021e51be25db031869afd
Kiefer King <kiefydking@yahoo.com>
2023-01-11 19:07:11 -0600

    adds the ingredients into an array

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,9 @@
 //  Adds the ingredient into the list as well as clears the bar for the next input
+var ingrdientsArray = [];
 function addIngredient(event) {
     event.preventDefault();
     var ingredient = document.getElementById("ingredient-input").value;
+    ingrdientsArray.push(ingredient);
     var list = document.getElementById("list-of-ingrdients");
     list.innerHTML += ingredient + "<br>";
     document.getElementById("ingredient-input").value = "";
