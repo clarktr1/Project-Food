301982dbbc686e3144a940ede70de775edcad7b7
Kiefer King <kiefydking@yahoo.com>
2023-01-11 19:41:11 -0600

    adding APIkeys

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,9 @@
+const apiKey = "02d88c0bd1614f2a831e95aa71af5f31";
+const apiUrl = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${apiKey}&ingredients=`;
+
 //  Adds the ingredient into the list as well as clears the bar for the next input and creates them into an array
 var ingrdientsArray = [];
+
 function addIngredient(event) {
     event.preventDefault();
     var ingredient = document.getElementById("ingredient-input").value;
