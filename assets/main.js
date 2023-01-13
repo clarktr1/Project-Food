267c42f08b7fd79648b9cbce7dd11437774d979e
Trey Clark <itreyclark@gmail.com>
2023-01-12 18:44:58 -0600

    test test

--- assets/main.js
+++ assets/main.js
@@ -14,9 +14,60 @@ function addIngredient(event) {
   }
 
 
+// Takes the list of ingredients and using an API finds 10 recipes that you can make with it
+function findRecipes(event) {
+    event.preventDefault();
+    const ingredients = ingrdientsArray.join(",");
+    fetch(apiUrl + ingredients)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            document.getElementById("list-of-ingredients").innerHTML = "";
+        });
+}
 
-
-
-
-  // Nutrition Count
+// bulma modal JS
+document.addEventListener('DOMContentLoaded', () => {
+    // Functions to open and close a modal
+    function openModal($el) {
+      $el.classList.add('is-active');
+    }
   
+    function closeModal($el) {
+      $el.classList.remove('is-active');
+    }
+  
+    function closeAllModals() {
+      (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+        closeModal($modal);
+      });
+    }
+  
+    // Add a click event on buttons to open a specific modal
+    (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+      const modal = $trigger.dataset.target;
+      const $target = document.getElementById(modal);
+  
+      $trigger.addEventListener('click', () => {
+        openModal($target);
+      });
+    });
+  
+    // Add a click event on various child elements to close the parent modal
+    (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+      const $target = $close.closest('.modal');
+  
+      $close.addEventListener('click', () => {
+        closeModal($target);
+      });
+    });
+  
+    // Add a keyboard event to close all modals
+    document.addEventListener('keydown', (event) => {
+      const e = event || window.event;
+  
+      if (e.keyCode === 27) { // Escape key
+        closeAllModals();
+      }
+    });
+  });
