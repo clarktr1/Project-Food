c007c1caab7327d04038ef5773a35a5e9e2528b5
Trey Clark <itreyclark@gmail.com>
2023-01-18 18:54:56 -0600

    adds new styling, working ingredients, and other things

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,29 @@
 const apiKey = "02d88c0bd1614f2a831e95aa71af5f31";
 const apiUrl = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${apiKey}&ingredients=`;
 
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Add a click event on each of them
+  $navbarBurgers.forEach( el => {
+    el.addEventListener('click', () => {
+
+      // Get the target from the "data-target" attribute
+      const target = el.dataset.target;
+      const $target = document.getElementById(target);
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      el.classList.toggle('is-active');
+      $target.classList.toggle('is-active');
+
+    });
+  });
+
+});
+
 //  Adds the ingredient into the list as well as clears the bar for the next input and creates them into an array
 var ingredientsArray = [];
 var numberofsavedRecipes = 0;
@@ -18,16 +41,24 @@ function addIngredient(event) {
     ingredientsArray.push(ingredient);
     var list = document.getElementById("list-of-ingredients");
     list.innerHTML += `
-    <li>
+    <li class="list-item delete-item">
     ${ ingredient }
     </li>`;
+    document.querySelectorAll('.delete-item').forEach(function(span) {
+      span.addEventListener('click', () =>
+       span.remove())
+    });
     document.getElementById("ingredient-input").value = "";
   }
+ 
+
 
 function saveItem(item){
   numberofsavedRecipes += 1
   localStorage.setItem ('savedRecipes', numberofsavedRecipes)
-  localStorage.setItem(numberofsavedRecipes, JSON.stringify(item))
+  localStorage.setItem(numberofsavedRecipes, item)
+  console.log(item);
+  return
   
 };
 
@@ -38,9 +69,10 @@ function findRecipes(event) {
     fetch(apiUrl + ingredients)
         .then(response => response.json())
         .then(data => {
-            // console.log(data);
-            document.getElementById("list-of-ingredients").innerHTML = "";
+
+            document.getElementById("list-of-ingredients").innerHTML = `<h2></h2>`;
             console.log(data);
+            console.log(ingredientsArray);
 
             //Places in Box
             var recipeCarousel = document.querySelector('.carousel');
@@ -61,24 +93,22 @@ function findRecipes(event) {
                 recipePicture.setAttribute('class', 'img-center');
 
                 var saverecipeBtn = document.createElement('button');
-                saverecipeBtn.setAttribute('class', 'button is-danger save-target')
+                saverecipeBtn.setAttribute('class', 'button is-danger save-target add-button')
                 saverecipeBtn.textContent = 'Add to Recipe List';
 
 
                 recipeCarousel.insertAdjacentElement("afterbegin", recipeList);
-                recipeList.insertAdjacentHTML("afterbegin", '<img src=' + recipeImage + '/>');
+                recipeList.insertAdjacentHTML("afterbegin", '<img src=' + recipeImage + '>');
                 recipeList.insertAdjacentElement("afterbegin", recipeName);
                 recipeList.insertAdjacentElement("beforeend", saverecipeBtn);
 
                 var SaveBtnLast = document.querySelectorAll('.save-target');
                 SaveBtnLast.forEach(function(button) {
-                  button.addEventListener('click', saveItem);
+                  button.addEventListener('click', () => saveItem(data[i]));
                 });
-          };
+              };
 
-          console.log(recipeList);
-
-          var carousel = document.querySelectorAll('.carousel').forEach(carousel => {
+           document.querySelectorAll('.carousel').forEach(carousel => {
             var items = carousel.querySelectorAll('.carousel-item');
             var carouselButton = Array.from(items, () => {
               return '<span class="carousel-button"></span>';
@@ -103,7 +133,8 @@ function findRecipes(event) {
                items[0].classList.add('carousel-item-selected');
                buttons[0].classList.add('carousel-button-selected');
               });
-
+              ingredientsArray = [];
+              console.log(ingredientsArray);
         });
 }
 
@@ -153,34 +184,6 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     });
   });
-
-//Carousel JS
-// var carousel = document.querySelectorAll('.carousel').forEach(carousel => {
-//   var items = carousel.querySelectorAll('.carousel-item');
-//   var carouselButton = Array.from(items, () => {
-//     return '<span class="carousel-button"></span>';
-//   });
-
-//   carousel.insertAdjacentHTML("beforeend", `
-//   <div class =carousel-nav>
-//   ${ carouselButton.join("") }
-//   </div>
-//   `);
-
-//   var buttons = carousel.querySelectorAll('.carousel-button');
-//   buttons.forEach((button, i) => {
-//     button.addEventListener('click', () => {
-//       items.forEach(item => item.classList.remove('carousel-item-selected'));
-//       buttons.forEach(button => button.classList.remove('carousel-button-selected'));
-      
-//       items[i].classList.add('carousel-item-selected');
-//       button.classList.add('carousel-button-selected');
-//       });
-//      });
-//      items[0].classList.add('carousel-item-selected');
-//      buttons[0].classList.add('carousel-button-selected');
-//     });
-
 
 
 
